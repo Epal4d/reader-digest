4e125c490e8a42ea6aad2cb5ab5587834fdcd32b
Evan Alford <evan.al4d@gmail.com>
2026-04-08 15:58:53 -0500

    changes made to review.py, fixed rating field to be 1-10

--- digestapi/models/review.py
+++ digestapi/models/review.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 from .book import Book
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Review(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="reviews")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    rating = models.IntegerField(max_length=5)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
     comment = models.CharField(max_length=100)
     date_posted = models.DateTimeField(auto_now_add=True)
