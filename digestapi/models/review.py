7abfc01fc42106ac474b316c2f8dee7e868dfc47
Evan Alford <evan.al4d@gmail.com>
2026-04-08 15:36:47 -0500

    bookCategory class added to module, fixed naming convention in review.py

--- digestapi/models/review.py
+++ digestapi/models/review.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 from .book import Book
 
 class Review(models.Model):
-    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     rating = models.IntegerField(max_length=5)
     comment = models.CharField(max_length=100)
     date_posted = models.DateTimeField(auto_now_add=True)
