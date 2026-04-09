c3bd763bc9d51d8ae5ef6178c8ce767ae3ed9ee1
Evan Alford <evan.al4d@gmail.com>
2026-04-09 10:39:49 -0500

    updated urls users.py create

--- digestapi/models/review.py
+++ digestapi/models/review.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-from .book import Book
 from django.core.validators import MinValueValidator, MaxValueValidator
+from .book import Book
 
 class Review(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="reviews")
